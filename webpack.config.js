69c34bc925b63a03636bae20d8219a0097910b4a
Cael Krigbaum <krigbaum.cael@gmail.com>
2018-09-18 12:56:29 -0700

    fixed paths to public to capitalize P in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     entry: './src/app.js',
     mode: 'development',
     output: {
-      path: path.join(__dirname,'public'),
+      path: path.join(__dirname,'Public'),
       filename: 'bundle.js'
     },
     module: {
@@ -41,7 +41,7 @@ module.exports = (env) => {
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname,'public'),
+      contentBase: path.join(__dirname,'Public'),
       historyApiFallback: true
     }
   };
