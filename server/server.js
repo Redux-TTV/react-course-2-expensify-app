559f25b110636382fcdbcb0232de16bf19518540
Cael Krigbaum <krigbaum.cael@gmail.com>
2018-09-18 12:44:30 -0700

    removed '/static' from app.use in server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var app = express();
 const publicPath = path.join(__dirname,'../Public');
 const port = process.env.PORT || 3000;
 
-app.use('/static', express.static(publicPath));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'Index.html'));
